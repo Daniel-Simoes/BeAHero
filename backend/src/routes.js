03f6014497a09f33fa9806b1c5865dfc88f38342
daniel.simoes <daniel.simoes@hotmail.com>
2020-03-25 01:33:55 +0000

    Pagination to Incidents

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,8 +4,11 @@ const express = require('express');
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileOngController = require('./controllers/ProfileOngController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
+
+routes.post('/sessions',SessionController.create);
 
 routes.get('/ongs',OngController.index);
 routes.post('/ongs',OngController.create);
