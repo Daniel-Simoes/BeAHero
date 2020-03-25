1689c7ad803876c582791ee02731a95a80697306
daniel.simoes <daniel.simoes@hotmail.com>
2020-03-25 01:09:10 +0000

    Method Delete to Incidents & Profile Controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
+const ProfileOngController = require('./controllers/ProfileOngController');
 
 const routes = express.Router();
 
@@ -11,5 +12,8 @@ routes.post('/ongs',OngController.create);
 
 routes.get('/incidents',IncidentController.index);
 routes.post('/incidents',IncidentController.create);
+routes.delete('/incidents/:id',IncidentController.delete);
+
+routes.get('/profile',ProfileOngController.index);
 
 module.exports = routes;
