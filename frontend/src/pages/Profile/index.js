9e4018b2b1364e57be8b6f3f3ad6014e58622cc0
daniel.simoes <daniel.simoes@hotmail.com>
2020-03-26 16:29:52 +0000

    Call Incidents to API with UseEffect

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 
+import api from '../../services/api';
 
 import Logo from '../../assets/logo.svg';
 
 import './styles.css';
 
 export default function Profile() {
+    const ongName = localStorage.getItem('ongName');
+    const ongId = localStorage.getItem('ongId');
+
+    const[ incidents, setIncidents ] = useState([]);
+
+    useEffect(() => {
+        api.get('profile', {
+            headers: {
+                Authorization: ongId,
+            }
+        }).then( response => {
+            setIncidents(response.data);
+        })
+    }, [ongId]);
+
   return (
     <div className="profile-container">
         <header>
         <img src={Logo} alt="Logo"/>
-            <span>Welcome</span>
+        <span>Welcome, {ongName}</span>
             <Link className="button" to="/incidents/new"> New Incidents</Link>
             <button type="button">
                 <FiPower size={18} color="#E02041" />
@@ -20,53 +36,21 @@ export default function Profile() {
         </header>
         <h1>Incidents</h1>
         <ul>
-            <li>
-                <strong>Incident:</strong>
-                <p>Incident Test</p>
-                <strong>Description:</strong>
-                <p>Description Test</p>
-                <strong>Value:</strong>
-                <p>$ 200</p>
-                <button type="button" >
-                    <FiTrash2 size={20} color="#A8A8B3" />
-                </button>
-            </li>
-
-            <li>
-                <strong>Incident:</strong>
-                <p>Incident Test</p>
-                <strong>Description:</strong>
-                <p>Description Test</p>
-                <strong>Value:</strong>
-                <p>$ 200</p>
-                <button type="button" >
-                    <FiTrash2 size={20} color="#A8A8B3" />
-                </button>
-            </li>
-
-            <li>
-                <strong>Incident:</strong>
-                <p>Incident Test</p>
-                <strong>Description:</strong>
-                <p>Description Test</p>
-                <strong>Value:</strong>
-                <p>$ 200</p>
-                <button type="button" >
-                    <FiTrash2 size={20} color="#A8A8B3" />
-                </button>
-            </li>
-
-            <li>
-                <strong>Incident:</strong>
-                <p>Incident Test</p>
-                <strong>Description:</strong>
-                <p>Description Test</p>
-                <strong>Value:</strong>
-                <p>$ 200</p>
-                <button type="button" >
-                    <FiTrash2 size={20} color="#A8A8B3" />
-                </button>
-            </li>
+           { incidents.map( incident => {
+               return (
+                    <li key={incident.id}>
+                        <strong>Incident:</strong>
+                        <p>{incident.title}</p>
+                        <strong>Description:</strong>
+                        <p>{incident.description}</p>
+                        <strong>Value:</strong>
+                        <p>{Intl.NumberFormat('en-UK', { style: 'currency', currency: 'EUR' }).format(incident.value)}</p>
+                        <button type="button" >
+                            <FiTrash2 size={20} color="#A8A8B3" />
+                        </button>
+                    </li>
+                )
+            } )}
         </ul>
     </div>
   );
